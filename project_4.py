406cbeecf150cfe6b59ffee70f4137c527430c25
liz728 <112724093+liz728@users.noreply.github.com>
2022-09-20 19:51:55 -0500

    Reused project 3 code for project 4

--- project_4.py
+++ project_4.py
@@ -1,12 +1,19 @@
+# This is our project 3 script that will preform the tasks required in the lab
+from pip._vendor import requests
+from os.path import exists
+
+# Set variables for static values, url, dates etc
+url = "https://s3.amazonaws.com/tcmg476/http_access_log"
+
 # How many requests were made on each day? 
 
 # How many requests were made on a week-by-week basis? Per month?
 
-What percentage of the requests were not successful (any 4xx status code)?
+# What percentage of the requests were not successful (any 4xx status code)?
 
-What percentage of the requests were redirected elsewhere (any 3xx codes)?
+# What percentage of the requests were redirected elsewhere (any 3xx codes)?
 
-What was the most-requested file?
+# What was the most-requested file?
 
-What was the least-requested file?
+# What was the least-requested file?
 
