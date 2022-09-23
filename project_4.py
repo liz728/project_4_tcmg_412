fa1d9539f9497c970e861508ae8426afe657a1bc
BloodRune3 <jacobgallegos00@gmail.com>
2022-09-23 09:45:12 -0500

    current logic

--- project_4.py
+++ project_4.py
@@ -1,12 +1,14 @@
 # This is our project 3 script that will preform the tasks required in the lab
 import requests
 from os.path import exists
+import re
 
 # Set variables for static values, url, dates etc
 url = "https://s3.amazonaws.com/tcmg476/http_access_log"
 
 # check for cached log file or pull the log file from apache server
 if not exists("total_log.txt"):
+    print("There was no log file found, searching the internet")
     log = requests.get(url)
     with open("total_log.txt", 'wb') as out:
         out.write(log.content)
@@ -14,7 +16,7 @@ if not exists("total_log.txt"):
 else:
     print("there was already a log on your local machine")
 
-
+total_log = open("total_log.txt", 'r')
 
 # How many requests were made on each day?
 
@@ -46,3 +48,9 @@ else:
 
 # split total log into by month log files
 
+# for line in total_log:
+#     month = re.findall('/.../', str(line))
+#     month = str(month) + ".txt"
+#     with open(month, 'w') as month_file:
+#         month_file.write(line)
+#     print(month)
