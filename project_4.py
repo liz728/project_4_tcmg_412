9ad6da47a5ff702000fc5bcb01112320db325739
BloodRune3 <jacobgallegos00@gmail.com>
2022-09-22 21:21:42 -0500

    Start!

--- project_4.py
+++ project_4.py
@@ -6,45 +6,43 @@ from os.path import exists
 url = "https://s3.amazonaws.com/tcmg476/http_access_log"
 
 # check for cached log file or pull the log file from apache server
-if not exists("project_3_output.txt"):
+if not exists("total_log.txt"):
     log = requests.get(url)
-    with open("project_3_output.txt", 'wb') as out:
+    with open("total_log.txt", 'wb') as out:
         out.write(log.content)
     print("we got the log from the internet")
 else:
     print("there was already a log on your local machine")
 
-# how many total requests have been made in the last 6 months, output to txt file
-with open("project_3_output.txt") as log_file:
-    lines = []
-    for line in log_file:
-        lines.append(line)
 
-start = 0
-date = "11/Apr/1995"
-for line in lines:
-    start += 1
-    if date in line:
-        break
-        
-# how many total requests have been made over the entire report, output to txt file
-lines_total = len(lines)
-
-# sixMonths is a variable used to count the lines between lines_total and start
-sixMonths = lines_total - start + 1
 
 # How many requests were made on each day?
 
-# How many requests were made on a week-by-week basis? Per month?
+
+
+# How many requests were made on a week-by-week basis?
+
+
+
+# Per month?
+
+
 
 # What percentage of the requests were not successful (any 4xx status code)?
 
+
+
 # What percentage of the requests were redirected elsewhere (any 3xx codes)?
 
+
+
 # What was the most-requested file?
+
+
 
 # What was the least-requested file?
 
 
-# output the data from the for loops
-print("In the report, there are " + str(lines_total) + " requests total and " + str(sixMonths) + " in the last six months.")
+
+# split total log into by month log files
+
