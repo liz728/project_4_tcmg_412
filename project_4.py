994765150950dbd8451a422e19fa370fe15ba9ec
Jacob Gallegos <jacobgallegos00@gmail.com>
2022-09-25 20:44:43 -0500

    split log

--- project_4.py
+++ project_4.py
@@ -1,4 +1,6 @@
 # This is our project 3 script that will preform the tasks required in the lab
+import collections
+
 import requests
 from os.path import exists
 import re
@@ -21,36 +23,75 @@ total_log = open("total_log.txt", 'r')
 # How many requests were made on each day?
 
 
-
 # How many requests were made on a week-by-week basis?
-
 
 
 # Per month?
 
 
-
 # What percentage of the requests were not successful (any 4xx status code)?
-
 
 
 # What percentage of the requests were redirected elsewhere (any 3xx codes)?
 
 
+# What was the most-requested file?j
+lines = ""
+for line in total_log:
+    lines = lines + line
+arg = re.findall("GET(.+)HTTP", str(lines))
+count = collections.Counter(arg)
+max_value = 0
+max_element = 0
 
-# What was the most-requested file?
+for element in list(count.keys()):
+    if count[element] >= max_value:
+        max_value = count[element]
+        max_element = element
 
-
+print(max_element)
 
 # What was the least-requested file?
+min_value = 0
+min_element = ''
 
+for element in list(count.keys()):
+    if count[element] == "":
+        pass
 
+    if count[element] <= min_value:
+        min_value = count[element]
+        min_element = element
+
+print(min_element)
+print('--------------')
 
 # split total log into by month log files
+def split_log():
+    # create empty list for storing each month
+    lines = []
+    # This variable lets us know if the new line from the log file has changed months
+    month_check = "Oct"
 
-# for line in total_log:
-#     month = re.findall('/.../', str(line))
-#     month = str(month) + ".txt"
-#     with open(month, 'w') as month_file:
-#         month_file.write(line)
-#     print(month)
+    # for loop runs through each line of log file
+    for line in total_log:
+        # uses regex to look for the pattern where the month is located
+        month = str(re.findall('/(...)/', str(line)))
+        # formatting so it is only Oct or whatever month it is, regex makes it ['Oct']
+        month = month[2:5]
+        # ensuring we don't run into function compatibility issues
+        line = str(line)
+
+        # Checks if month was updated, if it was, we know the month changed and we need to dump what we have
+        if month != month_check:
+            with open("Split_Files/" + month_check + ".txt", 'a+') as month_file:
+                # writes what was stored in lines and empties lines for next month
+                month_file.writelines(lines)
+                lines = []
+            month_file.close()
+
+        # These happen after we check if month changed so that we don't have some months seeping into wrong files
+        lines.append(line)
+        month_check = month
+
+    return "Logs are now split and located in the Split_Files directory"
